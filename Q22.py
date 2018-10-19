64914bc1e85a791681667f29b2dc88eba9c59c03
UmutTEKİN <umuttechin@gmail.com>
2018-10-19 11:02:59 +0300

    Update Q22.py

--- Q22.py
+++ Q22.py
@@ -1,24 +1,7 @@
-import string
-if __name__ == "__main__":
-    characters = string.ascii_uppercase
-    keys = []
-    values = []
-    name_keys = []
-    name_values = []
-    grand_total = 0
+from string import ascii_uppercase
+def score(word):
+    return sum(ascii_uppercase.index(c) + 1 for c in word.strip('"'))
 
-    for index in range(0,len(characters)):
-        keys.append(characters[index])
-        values.append(index+1)
-        
-    dict_aplha = dict(zip(keys,values))
-    f = open("name.txt")
-    elements = f.read().split(",")
-    sorted_list = sorted(elements)
-    
-    for range_index in range(0,len(sorted_list)):
-        total = 0
-        for char in (sorted_list[range_index])[1:-1]:
-            total += int(dict_aplha[char])
-        grand_total += total * (int(range_index)+1)
-print(grand_total)
+if __name__ == "__main__":
+    names = sorted(open("name.txt").read().split(","))
+    print(sum(ix*score(ax) for ix, ax in enumerate(names,1)))
