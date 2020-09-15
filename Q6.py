61e0398bf595b8941a32116528b72cbef520c71a
UmutTEKİN <umuttechin@gmail.com>
2020-09-16 01:09:31 +0300

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -7,3 +7,13 @@ for i in range(1,101):
     totalsq += i
     sqtotal += pow(i,2)
 print(pow(totalsq,2)-sqtotal)
+
+
+#Edited 2020.09.15
+
+def sqrtdiff(i):
+    sum = 0
+    for a in range(1, i + 1):
+        for b in range(a + 1, i + 1):
+            sum += 2 * a * b
+    return sum
